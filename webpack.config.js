d666b031f9f395e88580e8e276335fccd0c8da47
李达康 <hbxslyg@163.com>
2023-03-06 11:42:32 +0800

    feat: 配置 webpack 打包后文件自动引入到 html 中

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
