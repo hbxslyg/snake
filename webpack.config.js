3fd8b8f516a1dfe67642b1a5f588e3b010771746
李达康 <hbxslyg@163.com>
2023-03-07 12:37:50 +0800

    feat: 配置 typescript ;

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "js/bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          "ts-loader"
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
+  resolve: {
+		extensions: ['.ts', '.js']
+	},
 
   // 开发模式使用，方便查错误
   devtool: "inline-source-map",
